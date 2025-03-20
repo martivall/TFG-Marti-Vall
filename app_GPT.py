9c105264d509487737d70e484cefe5233f69acae
Martí <martivalloliveira@gmail.com>
2025-03-20 19:53:36 +0100

    box + fitxer punts

--- app_GPT.py
+++ app_GPT.py
@@ -11,6 +11,7 @@ import torch
 import cv2
 import supervision as sv
 from segment_anything import SamAutomaticMaskGenerator
+from segment_anything import SamPredictor
 from segment_anything import sam_model_registry
 import json
 
@@ -21,6 +22,7 @@ sam = sam_model_registry[MODEL_TYPE](checkpoint='sam_vit_b_01ec64.pth')
 sam.to(device=DEVICE)
 
 mask_generator = SamAutomaticMaskGenerator(sam)
+mask_predictor = SamPredictor(sam)
 
 external_stylesheets = [dbc.themes.BOOTSTRAP, "styles.css"]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -123,18 +125,54 @@ def parse_segmentation(contents):
     )
     return fig
 
+def parse_box_segmentation(contents, box):
+    img = image_string_to_PILImage(contents)
+    pix = np.array(img)
+    image_bgr = pix
+    image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
+    mask_predictor.set_image(image_rgb)
+    masks, scores, logits = mask_predictor.predict( box=box, multimask_output=True)
+    box_annotator = sv.BoxAnnotator(color=sv.Color.RED, color_lookup=sv.ColorLookup.INDEX)
+    mask_annotator = sv.MaskAnnotator(color=sv.Color.RED, color_lookup=sv.ColorLookup.INDEX)
+    detections = sv.Detections(
+    xyxy=sv.mask_to_xyxy(masks=masks),
+    mask=masks
+    )
+    detections = detections[detections.area == np.max(detections.area)]
+    segmented_image = mask_annotator.annotate(scene=image_bgr.copy(), detections=detections)
+    segmented_image = box_annotator.annotate(scene=segmented_image.copy(), detections=detections)
+    fig = px.imshow(segmented_image)
+    fig.update_xaxes(showgrid=False, ticks= '', showticklabels=False, zeroline=False)
+    fig.update_yaxes(
+        showgrid=False, scaleanchor="x", ticks= '', showticklabels=False, zeroline=False
+    )
+    fig.update_layout(
+        template=None,
+        dragmode="select"
+    )
+    return fig
+
 @app.callback(
     Output('stored-figure', 'data'),  # Guarda la figura procesada en dcc.Store
     Output('loading-flag', 'data'),  # Activa/Desactiva el estado de carga
     Input('segment-button', 'n_clicks'),
     Input('upload-image', 'contents'),
+    Input('output-image-upload', 'relayoutData')
 )
-def update_stored_figure(nclicks, content):
+def update_stored_figure(nclicks, content, relayout_data):
     if content is None:
         return {}, False
     if ctx.triggered_id == "segment-button":
         loading_flag = True
-        fig = parse_segmentation(content)
+        if "shapes" in relayout_data:
+            last_shape = relayout_data["shapes"][-1]
+            x0, y0 = int(last_shape["x0"]), int(last_shape["y0"])
+            x1, y1 = int(last_shape["x1"]), int(last_shape["y1"])
+            box = np.array([x0, y0, x1, y1])
+            print(box)
+            fig = parse_box_segmentation(content, box)
+        else:    
+            fig = parse_segmentation(content)
     elif ctx.triggered_id == "upload-image":
         is_loading = True
         fig = parse_contents(content)
